4ef7fe19ca6dff667db9cf2d85f8805f422c8230
BigCloudCMS <support@bigcloudcms.com>
2016-12-09 15:42:06 +0530

    Extra theme admin page conflict with affiliate plugin Solved
    by disabling jquery css file of affiliate plugin on Extra theme admin
    page

--- functions.php
+++ functions.php
@@ -7,3 +7,9 @@ add_action( 'wp_enqueue_scripts', 'elegant_enqueue_css' );
 include('editor/footer-editor.php');
 
 include('editor/login-editor.php');
+
+function theme_admin_enqueue($hook) {
+	wp_register_script('theme_admin_script', get_stylesheet_directory_uri() . '/theme-admin.js' );
+    wp_enqueue_script( 'theme_admin_script' );
+}
+add_action( 'admin_enqueue_scripts', 'theme_admin_enqueue' );
---
+++ theme_admin.js
@@ -0,0 +1,5 @@
+jQuery(function() {
+ if ( document.location.href.indexOf('?page=et_extra_options') > -1 ) {
+      jQuery('link[rel=stylesheet][href*="indeed-affiliate-pro"]').remove();
+    }
+});
