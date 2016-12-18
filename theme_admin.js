d47edc8913741009d369bce87ab361e7c86d70fd
BigCloudCMS <support@bigcloudcms.com>
2016-12-19 02:10:27 +0530

    ver 1.01
    Conflict with indeed-membership-pro

--- theme_admin.js
+++ theme_admin.js
@@ -1,5 +1,6 @@
 jQuery(function() {
  if ( document.location.href.indexOf('?page=et_extra_options') > -1 ) {
-      jQuery('link[rel=stylesheet][href*="indeed-affiliate-pro"]').remove();
+      jQuery('link[rel=stylesheet][href*="indeed-membership-pro"]').remove();
+     jQuery('link[rel=stylesheet][href*="indeed-affiliate-pro"]').remove();
     }
 });
